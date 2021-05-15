2d1672013369af68c3061b3f72c4796db89b7ff0
Jozef Lami <jozef.lami@dxheroes.io>
2021-05-15 20:34:37 +0200

    add vscode settings

--- src/first.test.ts
+++ src/first.test.ts
@@ -4,4 +4,12 @@ describe('first test', () => {
     it('test', async () => {
         expect(add(1, 1)).toBe(2);
     });
+
+    it.each([
+        [ 1, 1, 2 ],
+        [ 1, 2, 3 ],
+        [ 2, 1, 3 ],
+    ])('returns the result of adding %s to %s', (a, b, expected) => {
+        expect(add(a, b)).toBe(expected);
+    });
 });
